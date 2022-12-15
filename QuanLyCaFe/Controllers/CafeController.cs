07cfd2fcc5cb3724d19553c9041c8e4e4338c34e
duyth <duyth@DUYTHONG>
2022-12-15 20:36:58 +0700

    Add project files.

---
+++ QuanLyCaFe/Controllers/CafeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyCaFe.Controllers
+{
+    public class CafeController : Controller
+    {
+        // GET: Cafe
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult News()
+        {
+            return View();
+        }
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+        public ActionResult Cart()
+        {
+            return View();
+        } 
+        public ActionResult Product_item()
+        {
+            return View();
+        }
+        public ActionResult News_item()
+        {
+            return View();
+        }
+    }
+}
---
+++ QuanLyCaFe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuanLyCaFe.MvcApplication" Language="C#" %>
